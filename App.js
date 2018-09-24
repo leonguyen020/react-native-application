1c5528eb33b7e20067b8030a4667bd39cb393858
leonguyen020 <leonguyen020@gmail.com>
2018-09-24 20:52:00 +0700

    Remove default instruction

--- App.js
+++ App.js
@@ -10,13 +10,6 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import LoginPage from './src/components/login/login';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 // type Props = {};
 export default class App extends Component {
   render() {
