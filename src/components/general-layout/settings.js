0aa282d4eb100598ac598ec9650083dc7864b759
leonguyen020 <leonguyen020@gmail.com>
2018-09-24 21:29:10 +0700

    Add 2 dependencies: react-navigation && react-navigation-material-bottom-tabs

---
+++ src/components/general-layout/settings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class SettingsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Settings Page </Text>
+      </View>
+    );
+  }
+}
