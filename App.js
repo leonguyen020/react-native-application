d258d391e88858b2932cd46c0b64a5c59b840e00
leonguyen020 <leonguyen020@gmail.com>
2018-09-24 20:51:25 +0700

    Change dir name

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import LoginPage from './src/components/login-register/login';
+import LoginPage from './src/components/login/login';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
