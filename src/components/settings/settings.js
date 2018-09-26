a0e3561460cc5b64de8118d7df3c4f9f0a3db424
leonguyen020 <leonguyen020@gmail.com>
2018-09-26 21:32:04 +0700

    Styling settings page

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -22,12 +22,12 @@ export default class Settings extends Component {
     return (
       <View style={{ marginTop: 120 }}>
 
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: 180 }}>
+        <View style={[styles.rowContainer,styles.bottomMargin]}>
           <Touchable bgColor={{ backgroundColor: 'powderblue' }} onPress={this.onPress} icon="ios-people" text="Profile" />
           <Touchable bgColor={{ backgroundColor: 'skyblue' }} onPress={this.onPress} icon="ios-notifications" text="Notification" />
         </View>
 
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <View style={styles.rowContainer}>
           <Touchable bgColor={{ backgroundColor: 'steelblue' }} onPress={this.onPress} icon="ios-mail" text="Mail" />
           <Touchable bgColor={{ backgroundColor: 'lightsteelblue' }} onPress={this.onPress} icon="ios-trophy" text="Rankings" />
         </View>
@@ -37,3 +37,13 @@ export default class Settings extends Component {
   }
 }
 
+const styles = StyleSheet.create({
+  rowContainer:{
+    flex: 1, 
+    flexDirection: 'row', 
+    justifyContent: 'space-evenly',
+  },
+  bottomMargin:{
+    marginBottom: 180
+  }
+})
