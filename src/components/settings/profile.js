7bc71f8a880a498b8654fd4dafcb19c3257a4df1
Linh Do <s3689251@rmit.edu.vn>
2018-09-30 15:58:27 +0700

    get club profile from state

--- src/components/settings/profile.js
+++ src/components/settings/profile.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { 
+import {
   SectionList,
   View,
   Text,
   StyleSheet,
-  Dimensions
+  Dimensions,
+  ActivityIndicator
 } from 'react-native';
 import NavigationBar from 'react-native-navbar';
 
-const {width,height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 
 export default class Profile extends Component {
   static navigationOptions = { title: 'Profile' }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      club : {
+        _id: '1',
+        id: 1,
+        name: 'Innovation & Technology',
+        established: '04-05-2018',
+        pres: 3689251, //Account ID
+        vice: 3689251, //Account ID
+        chief: 3689251, //Account ID
+        currentPoint: 50,
+        currentRank: 1,
+        prevRank: 2,
+        staffID: 1234567
+      }
+    }
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -19,27 +39,28 @@ export default class Profile extends Component {
       title: 'Back',
       handler: () => navigate('SettingMenu'),
     };
-    
+
     const titleConfig = {
       title: 'Profile',
-      style:{paddingBottom:height/80}
+      style: { paddingBottom: height / 80 }
     };
+
     return (
       <View style={styles.container}>
         <NavigationBar
-          containerStyle={{backgroundColor:'#bdc3c7',height:height/9.5,marginBottom: height/30,}}
-          style={{paddingBottom:height/80}}
+          containerStyle={{ backgroundColor: '#bdc3c7', height: height / 9.5, marginBottom: height / 30, }}
+          style={{ paddingBottom: height / 80 }}
           title={titleConfig}
           leftButton={leftButtonConfig}
         />
         <View >
           <SectionList
             sections={[
-              { title: 'NAME', data: ['Innovation & Technology'] },
-              { title: 'ESTABLISHED', data: ['5/4/2018'] },
-              { title: 'PRESIDENT', data: ['Nguyen Huu Tri'] },
-              { title: 'VICE PRESIDENT', data: ['Nguyen Huu Tri'] },
-              { title: 'CHIEF OF FINANCE', data: ['Nguyen Huu Tri'] }
+              { title: 'NAME', data: [this.state.club.name] },
+              { title: 'ESTABLISHED', data: [this.state.club.established] },
+              { title: 'PRESIDENT', data: [this.state.club.pres] },
+              { title: 'VICE PRESIDENT', data: [this.state.club.vice] },
+              { title: 'CHIEF OF FINANCE', data: [this.state.club.chief] }
             ]}
             renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
             renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
