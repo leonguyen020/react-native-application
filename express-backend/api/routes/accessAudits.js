d32a450766f19195fc35ea9888bb94aff3503d76
usagi123 <maipham.quanghuy@gmail.com>
2018-10-04 13:43:28 +0700

    update middleware

--- express-backend/api/routes/accessAudits.js
+++ express-backend/api/routes/accessAudits.js
@@ -5,7 +5,7 @@ const checkAuth = require('../middleware/check-auth');
 const AccessAuditsController = require('../controllers/accessAudits');
 
 router.post('/', checkAuth, AccessAuditsController.post_audit);
-router.get('/', AccessAuditsController.get_all_audit);
-router.get('/:accessAuditId', AccessAuditsController.get_by_id);
+router.get('/', checkAuth, AccessAuditsController.get_all_audit);
+router.get('/:accessAuditId', checkAuth, AccessAuditsController.get_by_id);
 
 module.exports = router;
