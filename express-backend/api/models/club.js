96a2387e8fcf23f45c61b60b8d934e63e301e875
usagi123 <maipham.quanghuy@gmail.com>
2018-10-04 14:59:50 +0700

    Update some problems with Id

--- express-backend/api/models/club.js
+++ express-backend/api/models/club.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const clubSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    clubId: { type: String, require: true, unique: true },
     clubName: { type: String, require: true, unique: true },
     president: { type: String, require: true },
     vice: { type: String, require: true },
